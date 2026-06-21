0718ee7a1a5325c7519682abba71f2a694a7eff1
shamefuluser <nayefrancisco2205@gmail.com>
2026-06-20 23:32:53 -0400

    feat: implementar metodo subirFotoUsuario en ProfileViewModel

--- lib/presentation/viewmodels/profile_viewmodel.dart
+++ lib/presentation/viewmodels/profile_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -12,6 +13,9 @@ class ProfileViewModel extends ChangeNotifier {
   String get userName => _userName;
   String get userEmail => _userEmail;
 
+  String? _userPhotoUrl;
+  String? get userPhotoUrl => _userPhotoUrl;
+
   ProfileViewModel() {
     loadUserData();
   }
@@ -24,22 +28,60 @@ class ProfileViewModel extends ChangeNotifier {
       final user = _supabaseClient.auth.currentUser;
       if (user != null) {
         _userEmail = user.email ?? '';
-        
+
         // Obtiene los datos del usuario desde la tabla de usuarios
         final data = await _supabaseClient
             .from('usuarios')
-            .select('nombreUser, apellidoUser')
+            .select('nombreUser')
             .eq('auth_id', user.id)
             .maybeSingle();
 
         if (data != null) {
-          _userName = '${data['nombreUser']} ${data['apellidoUser']}'.trim();
+          _userName = (data['nombreUser'] ?? '').toString().trim();
         } else {
           _userName = user.userMetadata?['full_name'] ?? 'Usuario';
         }
       }
     } catch (e) {
       _userName = 'Error al cargar';
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  // Método para subir la foto de perfil del usuario
+  Future<void> subirFotoUsuario(File archivoImagen) async {
+    try {
+      _isLoading = true;
+      notifyListeners();
+
+      final user = _supabaseClient.auth.currentUser;
+      if (user == null) throw Exception("No hay ningún usuario autenticado.");
+      final String userId = user.id;
+
+      final String nombreArchivo = 'avatar_$userId.png';
+
+      await _supabaseClient.storage
+          .from('usuarios')
+          .upload(
+            nombreArchivo,
+            archivoImagen,
+            fileOptions: const FileOptions(cacheControl: '3600', upsert: true),
+          );
+
+      final String urlPublica = _supabaseClient.storage
+          .from('usuarios')
+          .getPublicUrl(nombreArchivo);
+
+      await _supabaseClient
+          .from('usuarios')
+          .update({'foto': urlPublica})
+          .eq('auth_id', userId);
+
+      _userPhotoUrl = urlPublica;
+    } catch (e) {
+      debugPrint('Error al subir la foto de perfil: $e');
     } finally {
       _isLoading = false;
       notifyListeners();
